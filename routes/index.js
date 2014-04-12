637196c4fbcec6ce7f814a9d547c1da817de989c
ryanjadhav <ryan.jadhav@gmail.com>
2014-04-12 10:41:24 -0700

    spotify api wrapper

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var spotify = require('./spotify');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  spotify.getTrackUrl('seventy times 7', function(err, url) {
+    console.log(url);
+  });
+  spotify.getTrackName('78HsNTKVNADOaGxS41OSW7', function(err, name) {
+    console.log(name);
+  });
+  res.render('index', { title: 'FstFwd' });
 });
 
 module.exports = router;
---
+++ routes/spotify.js
@@ -0,0 +1,53 @@
+var request = require('request');
+
+var BASE_WEB_URL = 'http://play.spotify.com';
+var BASE_API_URL = 'http://ws.spotify.com';
+
+var lookup = function(uri, cb) {
+  request(BASE_API_URL + '/lookup/1/.json?uri=' + uri, function(error, response, body) {
+    if (error) {
+      cb(error);
+    }
+    return cb(null, JSON.parse(body));
+   });
+};
+
+var search = function(query, cb) {
+  request(BASE_API_URL + '/search/1/track.json?q=' + query, function(error, response, body) {
+    if (error) {
+      cb(error);
+    }
+    return cb(null, JSON.parse(body));
+   });
+};
+
+var parseSearchForUrl = function(json) {
+  if (json && json.tracks && json.tracks.length) {
+    var href = json.tracks[0].href,
+        parts = href.split(':');
+    var playUrl = BASE_WEB_URL + '/track/' + parts[parts.length-1];
+    return playUrl;
+  } else {
+    return false;
+  }
+};
+
+var parseLookupForName = function(json) {
+  if (json && json.track) {
+    return json.track.name;
+  } else {
+    return false;
+  }
+};
+
+exports.getTrackUrl = function(query, cb) {
+  search(query, function(err, data) {
+    return cb(err, parseSearchForUrl(data));
+  });
+};
+
+exports.getTrackName = function(uri, cb) {
+  lookup('spotify:track:'+uri, function(err, data) {
+    return cb(err, parseLookupForName(data));
+  });
+};
