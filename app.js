7c8c212912145b41521cbd54c601c4a7fd7d1135
ryanjadhav <ryan.jadhav@gmail.com>
2014-04-15 23:16:16 -0700

    add translate route

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/translate', translate)
+app.use('/translate', translate);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
--- routes/translate.js
+++ routes/translate.js
@@ -3,8 +3,39 @@ var spotify = require('../lib/spotify');
 var rdio = require('../lib/rdio');
 var router = express.Router();
 
-router.post('/translate', function(req, res) {
-  console.log('req', req);
+router.post('/', function(req, res, next) {
+  var url = req.param('url');
+
+
+  if (url && !!url.match('rd.io')) {
+    var shortCode = url.split('/')[4];
+    rdio.getTrackName(shortCode, function(err, name) {
+      if (err) {
+        return next();
+      }
+      spotify.getTrackUrl(name, function(err, url) {
+        if (err) {
+          return next();
+        }
+        res.send({url: url});
+      });
+    });
+  } else if (url && !!url.match('spotify')) {
+    var shortCode = url.split('/')[4];
+    spotify.getTrackName(shortCode, function(err, name){
+      if (err) {
+        return next();
+      }
+      rdio.getTrackUrl(name, function(err, url) {
+        if (err) {
+          return next();
+        }
+        res.send({url: url});
+      });
+    });
+  } else {
+    res.send({url: ''});
+  }
 });
 
 module.exports = router;
