05deef32c047b3a3efeceb22c57eeab81effa568
ryanjadhav <ryan.jadhav@gmail.com>
2014-04-16 21:26:53 -0700

    remove console statements

--- routes/index.js
+++ routes/index.js
@@ -5,23 +5,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-
-  spotify.getTrackUrl('seventy times 7', function(err, url) {
-    console.log(url);
-  });
-
-  spotify.getTrackName('78HsNTKVNADOaGxS41OSW7', function(err, name) {
-    console.log(name);
-  });
-
-  rdio.getTrackName('QV5PjDddkJbX', function(err, name) {
-    console.log(name);
-  });
-
-  rdio.getTrackUrl('Artifice', function(err, url) {
-    console.log(url);
-  });
-
   res.render('index', { title: 'FstFwd' });
 });
 
